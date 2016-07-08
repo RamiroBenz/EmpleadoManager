c21715de871cbc1fed53d71faee6e2a75936bede
ITMaster Professional Training <itmasterprofessionaltraining@Mac-mini-de-ITMaster.local>
2016-07-07 21:30:09 -0300

    mi primer commit

---
+++ src/main/java/ar/com/itmaster/empleadomanager/Empleado.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ar.com.itmaster.empleadomanager;
+
+/**
+ *
+ * @author itmasterprofessionaltraining
+ */
+public class Empleado {
+    private String Nombre;
+    private Integer Edad;
+    private double Sueldo;
+    private Long id;
+    
+    
+    
+}
---
+++ src/main/java/ar/com/itmaster/empleadomanager/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ar.com.itmaster.empleadomanager;
+
+/**
+ *
+ * @author itmasterprofessionaltraining
+ */
+public class Main {
+    boolean esFeliz;
+    
+    public static void main(String[] args) {
+        Integer i=new Integer(2);
+        Integer i2 =new Integer(2);
+        Integer i3 = Integer.valueOf(2);
+        Integer i4 = Integer.valueOf(2);
+        Integer i5 = Integer.valueOf(2);
+        Integer i6 = Integer.valueOf(2);
+        Integer i7 = Integer.valueOf(2568);
+        Integer i8 = Integer.valueOf(2568);
+        System.out.println("Fin del Programa");
+        
+    }
+            
+            
+    
+}
